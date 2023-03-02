8bdf41d6cc1fba4d05644bb7198f95e398634347
Vadym Yevlanov <vad.evlanov@gmail.com>
2023-03-02 19:40:47 +0100

    added lessons' resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LessonsModule } from './lessons/lessons.module';
 
 @Module({
-  imports: [],
+  imports: [LessonsModule],
   controllers: [AppController],
   providers: [AppService],
 })
